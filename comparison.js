d6ed069844564730c2e41a0e9301fd9bb9c876f8
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-01-24 02:17:06 +0600

    First commit

---
+++ comparison.js
@@ -0,0 +1,11 @@
+const age=20;
+
+console.log( age >18);
+console.log(age === 20);
+console.log(age != 18);
+
+const hasId= true;
+
+console.log(age > 18 && hasId);
+console.log(age > 18 || hasId);
+console.log(!hasId); 
---
+++ simple-if-else.js
@@ -0,0 +1,27 @@
+let x=20;
+
+if(x>= 18){
+    console.log("you are Adult");
+}
+
+
+let age = 20;
+
+if (age >= 18) {
+  console.log("You can vote");
+} else {
+  console.log("You cannot vote");
+}
+
+// multiple conditions
+const marks = 88;
+
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 70) {
+  console.log("A");
+} else if (marks >= 60) {
+  console.log("A-");
+} else {
+  console.log("Below A-");
+}
