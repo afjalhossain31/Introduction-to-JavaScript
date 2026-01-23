50c7d444db8e8c347a0ab6202276d9f8531bd25f
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-01-24 02:25:19 +0600

    First commit

---
+++ Free-Drinks.js
@@ -0,0 +1,22 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 800; 
+
+if(burgerPrice > 500){
+    console.log("you get a free coke");
+}else{
+    console.log("Coke price:- is 30tk");
+}
+
+let burgerPrice1 = 400; 
+
+if(burgerPrice1 > 500){
+    console.log("you get a free coke");
+}else{
+    console.log("Coke price:- is 30tk");
+}
