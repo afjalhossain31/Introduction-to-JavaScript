9b87a9880ecf63cd80cbe3050ecdc7ab22d500d5
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-01-25 17:38:48 +0600

    added array and length

---
+++ Array.js
@@ -0,0 +1,12 @@
+//Create an array
+let number=[10,20,30,40,50,60,70,80,90,100];
+const fruits = ["Afjal", "Hosssain", "H"];
+
+
+//Accessing elements of an array
+console.log(number[0]); 
+console.log(fruits[1]); 
+console.log(number.length);
+console.log(fruits.length);
+console.log(number);
+console.log(fruits);
