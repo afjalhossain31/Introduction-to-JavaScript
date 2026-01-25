be67a0ed31f2bd414616e710e950ebb47ebd1edf
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-01-25 17:49:38 +0600

    added array  update

--- Array.js
+++ Array.js
@@ -6,6 +6,8 @@ const fruits = ["Afjal", "Hosssain", "H"];
 //Accessing elements of an array
 console.log(number[0]); 
 console.log(fruits[1]); 
+
+// Length of an array
 console.log(number.length);
 console.log(fruits.length);
 console.log(number);
@@ -19,3 +21,12 @@ const second = scores[1];
 const third = scores[2];
 
 console.log(first, second, third);
+
+// Set update by index
+
+let num=[1,2,3,4,5,6,7,8,9];
+
+num[3]=100;
+num[2]=300;
+
+console.log((num));
