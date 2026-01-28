8092b2d00e90f3b05a8424de8026d51c90d6e622
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-01-28 16:22:53 +0600

    added Variable  JavaScript

---
+++ Variable.js
@@ -0,0 +1,43 @@
+console.log("Afjal Hossain");
+
+// Variable
+
+/**
+ * Variable : container to store data
+ * Variable declaration keywords:
+ * 1. var
+ * 2. let
+ * 3. const
+ * 
+ * 
+ * ১। letters (a-z, A-Z)
+ * ২। numbers (0-9)
+ * ৩। underscore _
+ */
+
+/**
+ * variable naming convention
+ * 1. camelCase
+ * 2. PascalCase
+ * 3. snake_case
+ */
+
+var schoolRoll = 1; //camelCase - we use it
+var CollegeRoll = 2; //PascalCase
+var university_roll = 10; //snake_case
+
+
+// let and const : can not declare variable using same name
+/**
+ * let: reassign possible / Variable value changeable 
+ * const: reassign not possible / constant value
+ */
+let location = "Bhairab, Kishoreganj";
+const name = "Ahashan Habib Utsho";
+let age = 24;
+
+age = 25; // re-assign
+age = 26; // re-assign
+age = 27; // re-assign
+age = 23; // re-assign
+console.log(age);
