541427a7c227b65c8f04ede0bf635d400c8cc2ac
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-01-28 16:28:29 +0600

    added type-conversion  JavaScript

---
+++ type_conversion.js
@@ -0,0 +1,19 @@
+// converted string to number
+
+let age= 23;
+
+// let age2 =Number(age);
+let age2 =parseInt(age);
+console.log(age2);
+
+let cgpa= "4.54";
+console.log(parseFloat(cgpa));
+
+let floatsum=0.1 + 0.2;
+console.log(floatsum.toFixed(2));
+
+// converted number to string
+
+let roll= 50;
+// let rollString= roll.toString();
+console.log(String(roll));
