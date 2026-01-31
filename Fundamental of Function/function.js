1183b4128a0f952b8e8862b3f91b9ea19d463b2f
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-02-01 01:46:24 +0600

    added function

---
+++ Fundamental of Function/function.js
@@ -0,0 +1,25 @@
+// const price = 45;
+
+function fanOffkor(){
+    console.log("This is a function .");
+    console.log("click the switch to turn off the fan");
+
+}
+
+function brushTeeth(){
+    console.log("This is a function.");
+    console.log("brush your teeth two times a day");
+    console.log("morning and night");
+}
+
+fanOffkor();
+brushTeeth();
+
+// function add(num1, num2){
+function sum(x,y){
+    const total = x + y;
+    return total;
+}
+
+const total = sum(5,9);
+console.log(total);
