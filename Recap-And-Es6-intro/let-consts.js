3073fdb9d2730912fd3d4c31229fb5067a3b4b6f
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-02-27 16:06:30 +0600

    let consts.js

---
+++ Recap-And-Es6-intro/let-consts.js
@@ -0,0 +1,26 @@
+const name = 'Bangladesh ';
+// name = ' Germany';
+const countryName = 'BD_' + name;
+console.log(countryName);
+
+let price = 120;
+price =140;
+console.log(price);
+
+// var name22 = 'Bangladesh';
+
+const dress= ['Shirt', 'Pant', 'Shoe'];
+dress.push('belt');
+console.log(dress);
+
+const student ={
+    name: 'Afjal Hossain',
+    age: 24
+}
+
+// reassigning
+student.name = 'Afjal Hossain 2.0';
+student.age = 25;
+console.log(student);
+
+// Difference between var and let/const
