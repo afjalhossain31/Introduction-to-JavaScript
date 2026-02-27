3dc04ad52666c8f5b5c27cc9578f87bc7d14af5b
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-02-27 16:25:46 +0600

    ES6 Concepts

---
+++ Recap-And-Es6-intro/template-string.js
@@ -0,0 +1,29 @@
+const firstName = 'Afjal ';
+const lastName = "Hosssain";
+
+// const aboutMe = 'asdasf 
+
+
+const aboutMe = `My name is ${firstName}`;
+console.log(aboutMe);
+const giveMe = `give me money ${(10+30)*3+500}`
+console.log(giveMe);
+
+const fullName = firstName + ' ' + lastName;
+const fullName2 = `${firstName} ${lastName}`;
+
+// begtics --> ``
+
+function getCardHTML(name, description, price) {
+    const div = `
+        <div class="card">
+            <h2>${name.toUpperCase()} Borolox</h2>
+            <p>Price: $ ${price}</p>
+            <p>${description}</p>
+        </div>
+    `;
+
+    console.log(div);
+}
+// calling the function
+getCardHTML('iPhone 16', 'This is the latest iPhone model', 1099);
